e65846e1a0a1576059e97bcdde6072c50614528b
Ismail Sunni <imajimatika@gmail.com>
2019-05-23 12:35:03 +0200

    Minimal code to draw triangle.

--- main.cpp
+++ main.cpp
@@ -10,6 +10,18 @@
 #include <Qt3DRender/QCamera>
 #include <Qt3DExtras/QOrbitCameraController>
 
+#include <Qt3DExtras/QForwardRenderer>
+#include <Qt3DExtras/QPerVertexColorMaterial>
+#include <Qt3DRender/QGeometryRenderer>
+#include <Qt3DRender/QGeometry>
+#include <Qt3DRender/QAttribute>
+#include <Qt3DRender/QBuffer>
+#include <QPropertyAnimation>
+#include <Qt3DExtras/qt3dwindow.h>
+#include <Qt3DExtras/qorbitcameracontroller.h>
+
+#include<QDebug>
+
 int main(int argc, char *argv[])
 {
     // Application
@@ -58,6 +70,65 @@ int main(int argc, char *argv[])
     cuboidEntity->addComponent(cuboidTransform);
     cuboidEntity->addComponent(cuboidMaterial);
     cuboidEntity->setEnabled(true);
+
+
+    // Material
+    Qt3DRender::QMaterial *material = new Qt3DExtras::QPerVertexColorMaterial(rootEntity);
+    // Torus
+    Qt3DCore::QEntity *customMeshEntity = new Qt3DCore::QEntity(rootEntity);
+    // Transform
+    Qt3DCore::QTransform *transform = new Qt3DCore::QTransform;
+    transform->setScale(8.0f);
+    // Custom Mesh (TetraHedron)
+    Qt3DRender::QGeometryRenderer *customMeshRenderer = new Qt3DRender::QGeometryRenderer;
+    Qt3DRender::QGeometry *customGeometry = new Qt3DRender::QGeometry(customMeshRenderer);
+    Qt3DRender::QBuffer *vertexDataBuffer = new Qt3DRender::QBuffer(Qt3DRender::QBuffer::VertexBuffer, customGeometry);
+
+    QByteArray vertexBufferData;
+    // Three vertex, three float each
+    vertexBufferData.resize(3 * 3 * sizeof(float));
+
+    // Vertices
+    QVector3D v0(-0.5f, -0.5f, 0.0f);
+    QVector3D v1(0.5f, -0.5f, 0.0f);
+    QVector3D v2(0.0f,  0.5f, 0.0f);
+
+    QVector<QVector3D> vertices = QVector<QVector3D>() << v0 << v1 << v2;
+
+    // What is this step?
+    float *rawVertexArray = reinterpret_cast<float *>(vertexBufferData.data());
+    int idx = 0;
+    Q_FOREACH (const QVector3D &v, vertices) {
+        rawVertexArray[idx++] = v.x();
+        rawVertexArray[idx++] = v.y();
+        rawVertexArray[idx++] = v.z();
+    }
+
+    vertexDataBuffer->setData(vertexBufferData);
+
+    // Attributes
+    Qt3DRender::QAttribute *positionAttribute = new Qt3DRender::QAttribute();
+    positionAttribute->setAttributeType(Qt3DRender::QAttribute::VertexAttribute);
+    positionAttribute->setBuffer(vertexDataBuffer);
+    positionAttribute->setVertexBaseType(Qt3DRender::QAttribute::Float);
+    positionAttribute->setVertexSize(3);
+    positionAttribute->setByteOffset(0);
+    positionAttribute->setByteStride(3 * sizeof(float));
+    positionAttribute->setCount(3);
+    positionAttribute->setName(Qt3DRender::QAttribute::defaultPositionAttributeName());
+
+    customGeometry->addAttribute(positionAttribute);
+
+    customMeshRenderer->setInstanceCount(1);
+    customMeshRenderer->setIndexOffset(0);
+    customMeshRenderer->setFirstInstance(0);
+    customMeshRenderer->setPrimitiveType(Qt3DRender::QGeometryRenderer::Triangles);
+    customMeshRenderer->setGeometry(customGeometry);
+    customMeshRenderer->setVertexCount(3);
+
+    customMeshEntity->addComponent(customMeshRenderer);
+    customMeshEntity->addComponent(transform);
+    customMeshEntity->addComponent(material);
 
     view->setRootEntity(rootEntity);
     view->show();
